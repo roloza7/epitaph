f93afe06d6fb28f7c6ab379c4aa7f9f512cb3d33
Andrew Zhang <azhang377@gatech.edu>
2023-10-12 17:09:06 -0400

    working melee slash hitbox with rotation

---
+++ Assets/Scripts/Ability System/MeleeSlashHitbox.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeSlashHitbox : MonoBehaviour
+{
+    public GameObject parent;
+    public float damage;
+
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Enemy") {
+            Enemy enemy = other.GetComponent<Enemy>();
+            parent.GetComponent<Entity>().DealDamage(enemy, damage);
+        }
+        
+        // Debug.Log($"Hit {other.gameObject.name}");
+    }
+}
