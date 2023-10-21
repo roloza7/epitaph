15aa480470f8ee21dcf20ada128aae157098d9fb
Sahil Samar <sahilsamar031@gmail.com>
2023-10-21 16:15:51 -0400

    added more to item skeleton
    enabling/disabling interface

---
+++ Assets/Scripts/Currency System/BuffHealth.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffHealth : Item
+{
+    public override void firstActivation(Player player)
+    {
+
+    }
+}
--- Assets/Scripts/Currency System/Item.cs
+++ Assets/Scripts/Currency System/Item.cs
@@ -10,6 +10,9 @@ public class Item : ScriptableObject
     [SerializeField]
     protected string itemName; // name of the item
 
+    [SerializeField]
+    protected bool enabled;
+
     public int getCost()
     {
         return this.cost;
@@ -17,7 +20,37 @@ public class Item : ScriptableObject
 
     public virtual void activate(Player player)
     {
-        // do something
+        if (!enabled)
+        {
+            firstActivation(player);
+        }
+        enabled = true;
+    }
+
+    public virtual void deactivate(Player player)
+    {
+        enabled = false;
+        disableStatic(player);
+    }
+
+    public virtual void firstActivation(Player player)
+    {
+        // can use for static items such as adding health
+    }
+
+    public virtual void disableStatic(Player player)
+    {
+        // for temporary static buffs
+    }
+
+    public virtual float applyItemDamageTaken(float damageTaken, Entity current, Entity target)
+    {
+        return 0;
+    }
+
+    public virtual float applyItemDamageDealt(float damageDealt, Entity current, Entity target, HashSet<AbilityTag> tags)
+    {
+        return 0;
     }
 
 }
