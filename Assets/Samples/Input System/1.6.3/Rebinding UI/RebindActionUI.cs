87b9b8558c22562bb4062122df1e944cd043e090
IanCVal94 <86568417+IanCVal94@users.noreply.github.com>
2023-10-31 02:58:55 -0400

    now can successfully rebind controls
    still need to work on stoping duplicate controls and some checks before complete

--- Assets/Samples/Input System/1.6.3/Rebinding UI/RebindActionUI.cs
+++ Assets/Samples/Input System/1.6.3/Rebinding UI/RebindActionUI.cs
@@ -262,11 +262,14 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
                 m_RebindOperation = null;
             }
 
+            action.Disable();
+
             // Configure the rebind.
             m_RebindOperation = action.PerformInteractiveRebinding(bindingIndex)
                 .OnCancel(
                     operation =>
                     {
+                        action.Enable();
                         m_RebindStopEvent?.Invoke(this, operation);
                         m_RebindOverlay?.SetActive(false);
                         UpdateBindingDisplay();
@@ -275,6 +278,7 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
                 .OnComplete(
                     operation =>
                     {
+                        action.Enable();
                         m_RebindOverlay?.SetActive(false);
                         m_RebindStopEvent?.Invoke(this, operation);
                         UpdateBindingDisplay();
