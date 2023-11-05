fcd63f4db65e4c14fa1c116e4cfab01d29ceb7d4
IanCVal94 <86568417+IanCVal94@users.noreply.github.com>
2023-11-05 16:31:08 -0500

    check if works

--- Assets/Scripts/DialogLogic.cs
+++ Assets/Scripts/DialogLogic.cs
@@ -24,6 +24,7 @@ public class DialogLogic : MonoBehaviour
     UIAnimator UIAnimatorScript;
 
     StartConvo StartConvoScript;
+    
     // Start is called before the first frame update
     void Start()
     {
